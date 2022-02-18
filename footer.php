d5935000229635e4d7b9509798d20539314aaaf2
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-02-18 16:43:20 -0300

    changes

--- footer.php
+++ footer.php
@@ -32,5 +32,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	astra_body_bottom();    
 	wp_footer(); 
 ?>
+<!-- Botão Voltar ao Topo Deslizante -->
+<a id="subirTopo" class="voltar-ao-topo">^</a>
+
+<script type="text/javascript">
+   jQuery(document).ready(function(){
+      jQuery("#subirTopo").hide();
+      
+      Query('a#subirTopo').click(function () {
+          jQuery('body,html').animate({
+              scrollTop: 0
+          }, 800);
+          return false;
+      });
+      jQuery(window).scroll(function () {
+					if (jQuery(this).scrollTop() > 100) {
+            jQuery('#subirTopo').fadeIn();
+					} else {
+            jQuery('#subirTopo').fadeOut();
+         }
+      });
+    });
+ </script>
+
 	</body>
 </html>
